134bc01d0eaaf5af0503b3f7998ff808733f015a
Connor <kangruichen431@gmail.com>
2025-12-07 02:07:52 -0800

    Update database URI in config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
+import os
+
 class Config:
-    SECRET_KEY = "dev_secret_key" 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+    SECRET_KEY = "dev_secret_key"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
